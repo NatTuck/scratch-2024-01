10af9f27022e00e40745e3c0d5235e3091221770
Nat Tuck <nathaniel.tuck@plymouth.edu>
2024-02-21 14:19:14 -0500

    snowman character

---
+++ cs2370/11/string-nonsense.py
@@ -0,0 +1,44 @@
+
+# Comment blocks
+# require a # character
+# at the start of each line
+
+def print_print_statement():
+    '''This function will do
+    something and this is a multi-line
+    comment'''
+    return r'C:\ '
+
+xs = [0, 1, 2, 3, 4]
+text = "The quick brown fox jumps over the lazy dog."
+
+name = "Alice Jones"
+
+z = f'''
+Dear {name},
+
+I am a wealthy Nigerian price who needs to
+transfer money into the United Staes.
+
+Please email me your bank account information.
+
+Sincerely,
+
+King Leigit Guy
+'''
+
+
+
+email = "     bob@example.com   "
+
+
+
+file = open("pg65473.txt", encoding="utf8")
+count = 0
+for line in file:
+    count += 1
+print("lines =", count)
+
+
+
+
