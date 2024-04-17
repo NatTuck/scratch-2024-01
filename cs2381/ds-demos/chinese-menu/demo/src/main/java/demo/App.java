1bf0be15d9e0939e81dc36beb3997346eed76d62
Nat Tuck <nathaniel.tuck@plymouth.edu>
2024-04-17 09:55:13 -0400

    subtotal in cents

--- cs2381/ds-demos/chinese-menu/demo/src/main/java/demo/App.java
+++ cs2381/ds-demos/chinese-menu/demo/src/main/java/demo/App.java
@@ -13,10 +13,51 @@ import java.util.zip.GZIPInputStream;
 
 public class App {
     public static void main(String[] args) {
-        var lines = readMenuLines();
-        for (var line : lines) {
-            System.out.println("" + line);
+        var menuLines = readMenuLines();
+        var menu = new HashMap<String, MenuItem>();
+        for (var line : menuLines) {
+            var parts = line.split("\\t"); 
+            int cents = stringToCents(parts[2]);
+            var item = new MenuItem(parts[0], parts[1], cents);
+            menu.put(parts[0], item);
+            System.out.println(item);
         }
+
+        var con = System.console();
+
+        // Collect the order
+        con.printf("\n\nWelcome to Restaraunt\n\n");
+        var orderText = con.readLine("order> ");
+        con.printf("Your order: %s\n", orderText);
+
+        // Separate the menu codes
+        var orderCodes = orderText.split("\\s+");
+
+        // Find the menu items in the menu
+        var order = new ArrayList<MenuItem>();
+        for (var code : orderCodes) {
+            var item = menu.get(code);
+            order.add(item);
+        }
+
+        // Generate an order sub total
+        int subTotalCents = 0;
+        for (var item : order) {
+            subTotalCents += item.cents();
+        }
+        con.printf("subtotal = %d cents\n", subTotalCents);
+
+
+
+        // Calculate tax and total
+        // Print out our recipt
+    }
+
+    // "4.95" => 495
+    // "13.77" => 1377
+    static int stringToCents(String text) {
+        var parts = text.split("\\.");
+        return Integer.parseInt(parts[0] + parts[1]);
     }
 
     static List<String> readMenuLines() {
@@ -28,3 +69,6 @@ public class App {
     }
 }
 
+record MenuItem(String code, String name, int cents) {
+    // pass
+}
--- cs2381/ds-demos/starter-lab11/README.md
+++ cs2381/ds-demos/starter-lab11/README.md
@@ -35,3 +35,38 @@
  - Rather than just treating all edges as having a cost of 1,
    we can specify alternate cost (e.g. distance in miles)
 
+
+## Single Source Shortest Path
+
+Dijkstra's Algorithm:
+
+ - For each vertex (node) in the graph, we want to track
+   the following in a map:
+   - Estimated distance from starting node.
+     - Initially zero for the starting node
+     - Initially +inf for other nodes.
+   - Best previous node
+     - Initially null
+   - Done?
+     - boolean, initially false
+ - Keep a priority queue of nodes to explore
+ - For current node
+   - Check each neighbor node that isn't done for the following:
+     - If path through current node to that neighbor is 
+       better, update the map and add that node to the queue
+   - Once all neighbors checked, this node is done.
+
+A* Search:
+
+ - Take into account the coordinates of the verticies and
+   the distance to the destination in the priority queue.
+ - Specifically, rather than using distance from source as 
+   the score, we use:
+     - Distance from source + estimated distance to goal
+ - The estimated distance to goal is called the heuristic.
+ - We want the heuristic to underestimate the distance.
+ - Euclidian distance is pretty common.
+ - Fast, unless you're exploring a maze. Then still as fast
+   as you can really hope for.
+
+
